533841fde29b1cf23c8e51901aa30898e56666cd
kohnish <kohnish@gmx.com>
2024-09-02 19:08:40 +0200

    fix deps

--- vdiff.el
+++ vdiff.el
@@ -7,7 +7,6 @@
 ;; URL: https://github.com/justbur/emacs-vdiff
 ;; Version: 0.2.4
 ;; Keywords: diff
-;; Package-Requires: ((emacs "24.4") (hydra "0.13.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
